943de9997b9999d33943bf970881b869bbcfa124
Ivan Kanashchenko <ivan.kanashchenko@litslink.com>
2023-03-01 16:05:23 +0200

    auth requests

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
 import { validationResult } from "express-validator";
 
 import { registerValidator } from "./validators/auth";
+import UserModel from "./models/user";
 
 mongoose
-  .connect(
-    "mongodb+srv://ee1Solncev:Sinetko225348@testcluster.fewa1fo.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect("mongodb+srv://ee1Solncev:Sinetko225348@testcluster.fewa1fo.mongodb.net/blog?retryWrites=true&w=majority")
   .then(() => console.log("DB connected"))
   .catch((e) => console.log("DB Error", e));
 
@@ -16,12 +17,13 @@ const app = express();
 
 app.use(express.json());
 
-const getToken = (mail: string) =>
+const createToken = (id: string) =>
   jwt.sign(
     {
-      mail: mail,
+      _id: id,
     },
-    "secretKey"
+    "secretKey",
+    { expiresIn: "30d" }
   );
 
 interface RequestBody<T> extends Request {
@@ -38,31 +40,50 @@ interface IRegister extends ILogin {
   fullName: string;
 }
 
-app.post("/auth/login", (req: RequestBody<ILogin>, res: Response) => {
-  const token = getToken(req.body.mail);
+app.post("/auth/login", async (req: RequestBody<ILogin>, res: Response) => {
+  try {
+    const user = await UserModel.findOne({ mail: req.body.mail });
 
-  res.json({
-    success: true,
-    token,
-  });
+    if (!user) return res.status(404).json({ message: "user with this credentials not found" });
+
+    const isValidPass = await bcrypt.compare(req.body.password, user.password);
+
+    if (!isValidPass) return res.status(404).json({ message: "user with this credentials not found" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "something went wrong" });
+  }
 });
 
-app.post(
-  "/auth/register",
-  registerValidator,
-  (req: RequestBody<IRegister>, res: Response) => {
+app.post("/auth/register", registerValidator, async (req: RequestBody<IRegister>, res: Response) => {
+  try {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return res.status(400).json(errors.array());
     }
-    const token = getToken(req.body.mail);
+
+    const user = await new UserModel({
+      fullName: req.body.fullName,
+      mail: req.body.mail,
+      password: await bcrypt.hash(req.body.password, 10),
+    }).save();
+
+    const token = createToken(user._id.toString());
+
+    const { fullName, mail, _id } = user;
 
     res.json({
       success: true,
+      _id,
+      fullName,
+      mail,
       token,
     });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "something went wrong" });
   }
-);
+});
 
 app.listen(7777, () => console.log("Server Started"));
